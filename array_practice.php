eb4adcfaa08d2a645c119e7c242da58f89b20782
Krushna Chandra Nanda <139350558+Krushna-Nanda@users.noreply.github.com>
2023-12-01 13:10:51 +0000

    new update

---
+++ array_practice.php
@@ -0,0 +1,19 @@
+<?php
+// indexed array 
+
+$name = array( "krushna " , "kumar " , "sahoo " );
+
+echo $name[0];
+echo $name[1];
+echo $name[2];
+
+
+
+
+for ($i=0; $i < count($name) ; $i++) { 
+    # code...
+
+    echo $name[$i] . "\n";
+}
+
+?>
